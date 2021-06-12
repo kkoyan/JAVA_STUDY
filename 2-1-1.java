be67fe400c108d3ee83ed66ccdf2e8787197ef1c
kkoyan <71924575+kkoyan@users.noreply.github.com>
2021-06-13 08:38:29 +0900

    Create 2-1-1.java

---
+++ 2-1-1.java
@@ -0,0 +1,18 @@
+/*計算の文
+
+四則計算や変数に値を代入する。
+
+public class Main {
+  public static void main(String[] args) {
+    int a;  …変数宣言
+    int b;  …変数宣言
+    a = 20;  …代入
+    b = a + 5;  …足して代入
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
+実行結果：20  25
+
+
+*/
